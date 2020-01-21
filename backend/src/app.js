365d92955363496ac27248bd2c47ab7cb61e7ae2
t-heu <teuzin375@gmail.com>
2020-01-21 12:39:49 -0300

    add sentry

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,10 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
-//import * as Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import routes from './routes';
-//import sentryConfig from './config/sentry';
+import sentryConfig from './config/sentry';
 import 'express-async-errors';
 
 import './database';
@@ -13,22 +13,20 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-  
-    //Sentry.init(sentryConfig);
-  
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
-    //this.server.use(Sentry.Handlers.errorHandler());
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(cors());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
-    //this.server.use(Sentry.Handlers.errorHandler());
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
